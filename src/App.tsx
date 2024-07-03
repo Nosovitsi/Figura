c05b27838542f574c0d47ab4e639a5121ea1a9b6
Nosovitski Gleb <nosovitski.gleb@gmail.com>
2024-07-04 00:10:45 +0300

    some fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Greeting from './components/Greeting';
 import Column1 from './components/Column1';
-// import Column2 from './Column2';
+import Column2 from './components/Column2';
 // import Column3 from './Column3';
 
 
@@ -11,8 +11,7 @@ function App() {
       <Greeting />
       <div className="site">
          <Column1 />
-         {/* <Column2 />
-         <Column3 /> */}
+         <Column2 /> 
        </div>
 
     </div>
