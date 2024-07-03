abf0d8d06f981c4ec79a2afbe99f806caa2bb786
Nosovitski Gleb <nosovitski.gleb@gmail.com>
2024-07-03 20:31:51 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-// import Greeting from './components/Greeting';
-import Column1 from './Column1';
-import Column2 from './Column2';
-import Column3 from './Column3';
+import Greeting from './components/Greeting';
+// import Column1 from './Column1';
+// import Column2 from './Column2';
+// import Column3 from './Column3';
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Greeting /> */}
+      <Greeting />
       <div className="site">
-         <Column1 />
+         {/* <Column1 />
          <Column2 />
-         <Column3 />
+         <Column3 /> */}
        </div>
 
     </div>
