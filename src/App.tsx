e7918065e36415415abf4017262c642f732ddf05
Nosovitski Gleb <nosovitski.gleb@gmail.com>
2024-09-14 21:47:33 +0300

    fix back to websoc

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
-import React from 'react';
-import Layout from './components/Layout';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import Layout from "./components/Layout";
 
 const App: React.FC = () => {
-    return (
-        <div className="App">
-            <div className="container">
-                <Layout />
-            </div>
-        </div>
+  const socket = new WebSocket("ws://localhost:3001");
+
+  useEffect(() => {
+    socket.onopen = () => {
+      console.log("подключился");
+      getAppeals();
+    };
+
+    socket.onmessage = ({ data }) => {
+      const parsedData = JSON.parse(data);
+      console.log(parsedData);
+    };
+
+    return () => {};
+  }, []);
+
+  const getAppeals = () => {
+    console.log("getAppeals");
+    socket.send(
+      JSON.stringify({
+        action: "GET_APPEALS",
+        client_id: 1,
+      })
     );
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <Layout />
+      </div>
+    </div>
+  );
 };
 
 export default App;
