a96074ca834c3f154de4786747272a0f240e9ffd
Nosovitski Gleb <nosovitski.gleb@gmail.com>
2024-09-14 21:38:16 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,37 @@
-import React, { useEffect } from "react";
-import "./App.css";
-import Layout from "./components/Layout";
+import React from 'react';
+import Layout from './components/Layout';
+import './App.css';
 
 const App: React.FC = () => {
-  const socket = new WebSocket("ws://localhost:3001");
-
-  useEffect(() => {
-    socket.onopen = () => {
-      console.log("подключился");
-      getAppeals();
-    };
-
-    socket.onmessage = ({ data }) => {
-      const parsedData = JSON.parse(data);
-      console.log(parsedData);
-    };
-
-    return () => {};
-  }, []);
-
-  const getAppeals = () => {
-    console.log("getAppeals");
-    socket.send(
-      JSON.stringify({
-        action: "GET_APPEALS",
-        client_id: 1,
-      })
+    return (
+        <div className="App">
+            <div className="container">
+                <Layout />
+            </div>
+        </div>
     );
-  };
-
-  
-  return (
-    <div className="App">
-      <div className="container">
-        <Layout />
-      </div>
-    </div>
-  );
 };
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
